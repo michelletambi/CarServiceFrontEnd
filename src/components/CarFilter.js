d258d7c3db65725c28819b3e06b32b0f60039983
Michelle Tambi <michelletambi22@gmail.com>
2022-06-20 19:45:49 +0100

    installed axios

--- src/components/CarFilter.js
+++ src/components/CarFilter.js
@@ -2,46 +2,10 @@
 // User should be able to search and results should be filtered
 import axios from 'axios';
 import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const CarFilter = () => {
 
-    const [cars, setCars] = useState([])
-    const [model, setModel] = useState ("");
-    const [manufacturer, setManufacturer] = useState("");
-
-//          ----- java code for min / max price forms -----
-
-    // if (minPrice > maxPrice){
-    //     throw new Exception("Minimum price must be lower than maximum price!");
-    // }
-
-    // List<Product> priceRange = productRepository.findByPriceGreaterThanEqualAndPriceLessThanEqual(minPrice, maxPrice);
-    // List<Product> result = inStock.stream().filter(priceRange::contains).collect(Collectors.toList());
-
-    const searchByModel = useEffect( () => {
-        axios.get(`http://127.0.0.1:8080/filter_products/${model}`)
-        .then(res => {
-            // .filter()
-            const model = res.data;
-            setModel(model);})
-            .catch((err) => console.log("We do not stock this product"));
-        }, []);
-
-        function SearchByManufacturer() { useEffect( () => {
-            axios.get(`http://127.0.0.1:8080/filter_products/${manufacturer}`)
-            .then(res => {
-                // .filter()
-                const manufacturer = res.data;
-                setManufacturer(manufacturer);})
-                .catch((err) => console.log("We do not stock this product"));
-    
-            }, [])}
-
-    const button = document.querySelector("#button");
-
-    const handleButtonClick = (SearchByManufacturer) => {
-        button.addEventListener("click", handleButtonClick);
-    }
 
     
 
