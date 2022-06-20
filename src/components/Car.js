880e23f5ed83884f4c9f94a75dc70390bf62d138
Hamza Abdilahi <hamzacodes97@gmail.com>
2022-06-20 09:55:13 +0100

    Co-authored-by: michelletambi <michelletambi@users.noreply.github.com> Co-authored-by: scottiee-jc <scottiee-jc@users.noreply.github.com>

---
+++ src/components/Car.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {useState, useEffect} from "react";
+import axios from "axios";
+
+const Car = () => {
+
+}
+
+export default Car;
---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,18 @@
+
+const HomeContainer = () => {
+
+
+
+    return (
+        <>
+        <h1>Cars</h1> 
+        <img src = "" alt = "Cars for free logo"/>
+        <p> we buy any carz4free </p>
+        </>
+
+
+    );
+}
+
+
+export default HomeContainer;
