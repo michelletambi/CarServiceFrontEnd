519d318f9a4ef5b751e57c26e936c6b6b1969e68
Hamza Abdilahi <hamzacodes97@gmail.com>
2022-06-22 16:44:57 +0100

    fixes

--- src/containers/ContactContainer.js
+++ src/containers/ContactContainer.js
@@ -7,6 +7,7 @@ const ContactContainer = () => {
     return (
         <>
             <NavBar/>
+            
                 <h3 id="Contactform">Contact Form</h3>
             <div class="containerForm">
                 <form>
@@ -26,29 +27,8 @@ const ContactContainer = () => {
                 </form>
             </div>
 
-            <footer className="footer"> 
-                <hr/>
-                 <ul className="helpline">
-                    <strong> Help and Support</strong>
-                    <li><i class="fa-solid fa-phone"></i> Call: 0800 1110 66</li>
-                    <li><i class="fa-solid fa-inbox"></i> Email: Carz4Free@Carz.com </li>
-                </ul>
-                <ul className="socials"> 
-                    <li>Contact Us</li>
-                    <li><i class="fa-brands fa-twitter"></i></li>
-                    <li><i class="fa-brands fa-facebook"></i></li>
-                    <li><i class="fa-brands fa-instagram"></i></li>
-                </ul>
-                <ul className="Legalstuff"> 
-                    <li> Terms and conditions </li>
-                    <li> Privacy Policy </li>
-                    <li> Cookie Settings </li>
-                    <li> Legal </li>
-                </ul>
-                    <p className="copyrights"> 
-                        © 2022 Carz4free Ltd. All rights reserved. 
-                    </p>
-            </footer>
+            <Footer/>
+            
         </>
     );
 }
