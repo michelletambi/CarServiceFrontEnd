7e974bc63eb8d9ba6e5a4f7ed1bc8a7cf2e21b59
Hamza Abdilahi <hamzacodes97@gmail.com>
2022-06-22 16:43:26 +0100

    fixes

--- src/containers/BasketContainer.js
+++ src/containers/BasketContainer.js
@@ -25,31 +25,7 @@ const BasketContainer = () => {
                     <p>Total: <span class="price"><b>£</b></span></p>
                 </div>
             </div>
-            <Footer />
-                {/* <footer className="footer"> 
-                <hr/>
-                 <ul className="helpline">
-                    <strong> Help and Support</strong>
-                    <li><i class="fa-solid fa-phone"></i> Call: 0800 1110 66</li>
-                    <li><i class="fa-solid fa-inbox"></i> Email: Carz4Free@Carz.com </li>
-                </ul>
-
-                <ul className="socials"> 
-                    <li>Contact Us</li>
-                    <li><i class="fa-brands fa-twitter"></i></li>
-                    <li><i class="fa-brands fa-facebook"></i></li>
-                    <li><i class="fa-brands fa-instagram"></i></li>
-                </ul>
-                <ul className="Legalstuff"> 
-                    <li> Terms and conditions </li>
-                    <li> Privacy Policy </li>
-                    <li> Cookie Settings </li>
-                    <li> Legal </li>
-                </ul>
-                    <p className="copyrights"> 
-                        © 2022 Carz4free Ltd. All rights reserved. 
-                    </p>
-            </footer> */}
+                <Footer/>
         </>
     );
 }
