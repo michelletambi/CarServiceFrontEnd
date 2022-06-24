b9a8aabd3e7284f986b0d47740d837646451fa77
Hamza Abdilahi <hamzacodes97@gmail.com>
2022-06-24 09:08:28 +0100

    validation

--- src/containers/ContactContainer.js
+++ src/containers/ContactContainer.js
@@ -7,26 +7,32 @@ import Footer from "../components/Footer";
 const ContactContainer = () => {
     return (
         <>
+       
+
             <header>
                 <NavBar/>
             </header>
             <h3 id="title">Contact Us!</h3>
             <h4 id="headingCenter">If you have any issues or questions, we'd love to hear from you</h4>
-            <main className="elementBody">
-                
+            <main className="elementBody" action="/action.php" method="post">
+
             <div>
-                    <form className="containerForm">
+                    <form className="containerForm" >
                         <label id="headingCenter">First Name</label>
-                        <input type="uname" placeholder="Your first name.."></input>
+                        <input type="uname" placeholder="Your first name.." required ></input>
 
                         <label id="headingCenter"> Last Name </label>
-                        <input type="uname" placeholder="Your last name.."></input>
+                        <input type="uname" placeholder="Your last name.." required ></input>
+                        
+
 
                         <label id="headingCenter">Email</label>
-                        <input type="uname" placeholder="Your Email.."></input>
+                        <input type="uname" placeholder="Your Email.." required></input>
+                        
+
 
                         <label id="headingCenter">Subject</label>
-                        <textarea className="subject" name="subject" placeholder="Send us a message .."></textarea>
+                        <textarea className="subject" name="subject" placeholder="Send us a message .." required></textarea>
 
                         <input type="submit" value="submit"></input>
                     </form>
