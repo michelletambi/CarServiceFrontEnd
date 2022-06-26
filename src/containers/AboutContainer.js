ea75d1aa807256f082b7fd662c8d13ef0dd88f09
Michelle Tambi <michelletambi22@gmail.com>
2022-06-26 17:44:54 +0100

    m

--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -16,7 +16,7 @@ const AboutContainer = () => {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam quis enim lobortis scelerisque. Ornare lectus sit amet est placerat in egestas erat imperdiet. Sagittis purus sit amet volutpat consequat mauris. Est lorem ipsum dolor sit amet consectetur adipiscing. Hac habitasse platea dictumst quisque sagittis purus sit. Netus et malesuada fames ac turpis egestas integer eget aliquet. Vestibulum lorem sed risus ultricies tristique nulla. Suscipit tellus mauris a diam maecenas sed. Ut consequat semper viverra nam libero justo. Ac turpis egestas maecenas pharetra. Amet commodo nulla facilisi nullam vehicula. Et malesuada fames ac turpis egestas. Arcu cursus vitae congue mauris rhoncus aenean vel. Eget dolor morbi non arcu risus quis varius quam. Etiam sit amet nisl purus in mollis nunc. Neque vitae tempus quam pellentesque nec nam aliquam sem et.
 
             </h4>
-            <CarList/>
+            {/* <CarList/> */}
             <Footer />
         </>
     );
